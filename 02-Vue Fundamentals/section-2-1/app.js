578b97dd6ec746dc30684c015cc596fee8b185b8
axzed <waxwc1491224820@gmail.com>
2023-02-01 20:21:58 +0800

    Outputting Raw HTML

--- 02-Vue Fundamentals/section-2-1/app.js
+++ 02-Vue Fundamentals/section-2-1/app.js
@@ -4,6 +4,7 @@ const vm = Vue.createApp({
       firstName: 'John',
       lastName: 'Doe',
       url: 'https://www.google.com',
+      raw_url: '<a href="https://www.google.com" target="_blank">Google</a>'
     }
   },
   // v-bind 用法
