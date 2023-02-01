e40fa778b4a39bcde95a0db7d0b490728afc1d0e
axzed <waxwc1491224820@gmail.com>
2023-02-01 20:06:47 +0800

    data binding

--- 02-Vue Fundamentals/section-2-1/app.js
+++ 02-Vue Fundamentals/section-2-1/app.js
@@ -1,8 +1,26 @@
-Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
       firstName: 'John',
       lastName: 'Doe'
     }
+  },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    }
   }
 }).mount('#app');
+
+// setTimeout(() => {
+//   vm.firstName = 'xue';
+// }, 2000)
+
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: 'xue',
+//       lastName: 'wenchao'
+//     }
+//   }
+// }).mount('#app2');
