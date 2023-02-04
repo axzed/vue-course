e188553c96df4b621e9a1f1508ed6b2b6b71bb51
axzed <waxwc1491224820@gmail.com>
2023-02-04 19:27:25 +0800

    Finish Chapter4 Beyond the Fundamental study

---
+++ 04-Beyond the Fundamental/section-4-1/app.js
@@ -0,0 +1,61 @@
+let vm = Vue.createApp({
+  // data() {
+  //   return {
+  //     message: "Hello world!"
+  //   }
+  // },
+  // template: `{{ message }}`,
+  // beforeCreate() {
+  //   console.log('beforeCreate() function called!', this.message)
+  // },
+  // created() {
+  //   console.log('created() function called!', this.message)
+  // },
+  // beforeMount() {
+  //   console.log('beforeMount() function called!', this.$el)
+  // },
+  // mounted() {
+  //   console.log('mounted() function called!', this.$el)
+  // },
+  // beforeUpdate() {
+  //   console.log('beforeUpdate() function called!')
+  // },
+  // updated() {
+  //   console.log('updated() function called!')
+  // },
+  // beforeUnmount() {
+  //   console.log('beforeUnmount() function called!')
+  // },
+  // unmounted() {
+  //   console.log('unmounted() function called!')
+  // }
+})
+
+vm.component('hello', {
+  template: `<h1>{{ message }}</h1>`,
+  data() {
+    return {
+      message: `Hello world!`
+    }
+  }
+})
+
+vm.mount('#app')
+
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
+    }
+  },
+  render() {
+    return Vue.h(
+        `h1`,
+        this.message
+    )
+  }
+}).mount('#app2')
+
+// setTimeout(() => {
+//   vm.mount('#app')
+// }, 3000)
